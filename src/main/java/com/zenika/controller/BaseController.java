f56511b045a31c5a1ab0461c66b6134464e8fdda
l0rd <mario.loriedo@gmail.com>
2015-04-03 14:11:28 +0200

    Hello Be Zend

--- src/main/java/com/zenika/controller/BaseController.java
+++ src/main/java/com/zenika/controller/BaseController.java
@@ -17,7 +17,7 @@ public class BaseController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
  
-		model.addAttribute("message", "Welcome");
+		model.addAttribute("message", "Welcome Be Zend");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
  
